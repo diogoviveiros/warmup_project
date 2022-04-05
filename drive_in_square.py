abfcd11a8d5909058820cd62491e77ac97fe963d
diogoviveiros <diogopviveiros@gmail.com>
2022-04-04 23:53:16 -0500

    Update drive_in_square.py

--- drive_in_square.py
+++ drive_in_square.py
@@ -22,31 +22,37 @@ class DriveSquare(object):
             angular=Vector3(0, 0, 0)
         )
 
-    # setup the Twist message that will send the Turtlebot to turn 90 degrees
+        # setup the Twist message that will send the Turtlebot to turn 90 degrees
         turn_twist = Twist(
             linear=Vector3(0, 0, 0),
             angular=Vector3(0, 0, math.pi/2)
         )
+        
+        # setup the Twist message that will make the Turtlebot just stop
 
         stop_twist = Twist(
             linear=Vector3(0, 0, 0),
             angular=Vector3(0, 0, 0)
         )
 
+        #Make Robot repeat this motion four times in order to make a complete square path
         for _ in range(4):
 
             # allow the publisher enough time to set up before publishing the first msg
             rospy.sleep(1)
 
-            # publish the message
+            # publish the message to move forwards, and do this for 3 seconds
             self.robot_movement_pub.publish(forward_twist)
 
             rospy.sleep(3)
+            
+            #Publish the message to make the robot stop, then wait for 1 second. 
 
             self.robot_movement_pub.publish(stop_twist)
 
             rospy.sleep(1)
 
+            #Make the robot turn 90 degrees, then wait 1.1 seconds (slightly over 1 second to compensate for frictions and other real world effects)
             self.robot_movement_pub.publish(turn_twist)
 
             rospy.sleep(1.1)
